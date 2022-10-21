e4866721d59ae235ef3c3e78a17235862b643345
phamthehieu <111071418+phamthehieu@users.noreply.github.com>
2022-10-21 15:32:20 +0700

    lan2

--- js_Quan_Ly.js
+++ js_Quan_Ly.js
@@ -1,4 +1,5 @@
-let listProduct = ['Sony Xperia', 'Samsung Galaxy', 'Nokia 6', 'Xiaomi Redmi Note 4', 'Apple iPhone 6S']
+let listProduct = ['Sony Xperia', 'Samsung Galaxy', 'Nokia 6', 'Xiaomi Redmi Note 4', 'Apple iPhone 6S'];
+
 function displayAll() {
     let str = ` <tr> 
             <td>Product Name</td>
@@ -9,31 +10,35 @@ function displayAll() {
         str += `<tr>
         <td>${listProduct[i]}</td>
         <td><button onclick="showFromEdit(${i})">Edit</button></td>
-        <td><button onclick="deleteProduct(${i})">Delete</button></td>
+        <td><button onclick="deleteProDuct(${i})">Delete</button></td>
     </tr>`
         document.getElementById("displayArena").innerHTML = str;
 
     }
 }
+
 displayAll();
+
 function find() {
-    let productName = document.getElementById("add").value;
+    let proDuctName = document.getElementById("add").value;
     document.getElementById("add").value = "";
-    listProduct.push(productName);
-    displayAll();
+    listProduct.push(proDuctName);
+    displayAll()
 }
-function deleteProduct(inDex) {
+
+function deleteProDuct(inDex) {
     listProduct.splice(inDex, 1);
     displayAll();
 }
+
 function showFromEdit(inDex) {
     let arr = "";
-    arr = '<input type="text" id="editProduct" value="'+listProduct[inDex]+'"><button onclick="saveProduct(' + inDex + ')">Edit</button>'
-    document.getElementById('edit').innerHTML = arr;
-}
-function saveProduct(indDex) {
-        listProduct[indDex] = document.getElementById("editProduct").value;
-        document.getElementById("edit").innerHTML = "";
-        displayAll()
+    arr = `<input type="text" id="editProduct" value="${listProduct[inDex]}"><button onclick="saveProduct(${inDex})">Edit</button>`;
+    document.getElementById("edit").innerHTML = arr;
 }
 
+function saveProduct(inDex) {
+    listProduct[inDex] = document.getElementById("editProduct").value;
+    document.getElementById("edit").innerHTML = "";
+    displayAll();
+}
